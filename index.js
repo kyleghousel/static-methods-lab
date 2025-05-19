f03e43d6996f19212469ca358be9c862814e511b
Kyle Housel <kylegranthousel@gmail.com>
2025-05-19 12:08:32 -0400

    Create static methods

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
-  //add static methods here
+
+  constructor (string) {
+    this.string = string
+  }
+
+  static capitalize(word) {
+    return word[0].toUpperCase() + word.slice(1)
+  }
+
+  static sanitize(word) {
+    return word.replace(/[^A-Za-z0-9\-'\s]/g, "")
+  }
+
+  static titleize(sentence) {
+    const noCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    return sentence
+      .toLowerCase()
+      .split(" ")
+      .map((word, index) => {
+        if (index === 0 || !noCap.includes(word)) {
+          return word[0].toUpperCase() + word.slice(1);
+        } else {
+          return word;
+        }
+      })
+      .join(" ");
+  }
 }
