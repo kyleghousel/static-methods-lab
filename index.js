ede1e92d54f153df7329cf45c44a526afe329f51
Kyle Housel <kylegranthousel@gmail.com>
2025-05-19 13:22:50 -0400

    Clean syntax

--- index.js
+++ index.js
@@ -13,18 +13,18 @@ class Formatter {
   }
 
   static titleize(sentence) {
-    const noCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const noCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
 
     return sentence
       .toLowerCase()
       .split(" ")
       .map((word, index) => {
         if (index === 0 || !noCap.includes(word)) {
-          return word[0].toUpperCase() + word.slice(1);
+          return word[0].toUpperCase() + word.slice(1)
         } else {
-          return word;
+          return word
         }
       })
-      .join(" ");
+      .join(" ")
   }
 }
